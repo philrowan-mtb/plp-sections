6d41c2b1abb519da2517235d9f9fb204a79f9f66
Philip Rowan <phil.rowan@catchco.com>
2019-04-12 19:52:32 -0500

    sections

---
+++ WebApplication7/PageSection.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7
+{    
+    public class FilterSection
+    {
+        public string Title { get; set; }
+        public IList<FilterOption> Options { get; set; }
+
+        public FilterSection(string title, params string[] options)
+        {
+            Title = title;
+            Options = new List<FilterOption>();
+            if (options != null)
+            {
+                foreach (var option in options)
+                {
+                     Options.Add(new FilterOption(option));
+                }
+            }
+        }
+    }
+
+    public class FilterOption
+    {
+        public string Name { get; set; }
+
+        public FilterOption(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class CategorySection
+    {
+        public string Title { get; set; } = "Categories";
+
+        public IList<CategoryNode> Categories { get; set; }
+
+        public CategorySection()
+        {
+            Categories = new List<CategoryNode>();
+        }
+
+        public void Add(CategoryNode node)
+        {
+            Categories.Add(node);
+        }
+    }
+
+    public class CategoryNode
+    {
+        public string Title { get; set; }
+
+        public IList<CategoryNode> Categories { get; set; }
+
+        public CategoryNode(string title, params string[] childrenNodeTitles)
+        {
+            Title = title;
+            Categories = new List<CategoryNode>();
+            if (childrenNodeTitles != null)
+            {
+                foreach (var c in childrenNodeTitles)
+                {
+                    Categories.Add(new CategoryNode(c));
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication7/Pages/ListingPage.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication7.Pages
+{
+    public class ListingPageModel : PageModel
+    {
+        public CategorySection Categories { get; set; }
+        public IList<FilterSection> Filters { get; set; }
+        public void OnGet()
+        {
+            Categories = new CategorySection();
+            Categories.Add(new CategoryNode("Hardbaits", "Crankbaits", "Rip Baits"));
+            Categories.Add(new CategoryNode("Softbaits", "Worms", "Creatures", "Fish-like things"));
+            Categories.Add(new CategoryNode("Jigs", "Heads", "Skirts"));
+            
+            Filters = new List<FilterSection>();
+            Filters.Add(new FilterSection("Brand", "Rapala", "Karl's Amazing Baits", "Yo-Zuri"));
+            Filters.Add(new FilterSection("Size", "2\"", "2.5\"", "3\""));                
+            Filters.Add(new FilterSection("Color", "Red", "Green", "Yellow"));
+        }
+    }
+}
